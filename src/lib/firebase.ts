c547dfe8777bdda857d64aafc57dc45a823ce6b7
Lok <chanlokk97@gmail.com>
2026-07-05 19:45:43 +0200

    Update 2026-07-05 19:45

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp, getApps } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAM954tO7yIoC395MHHVWyIvqPPfIWZRzo",
+  authDomain: "courtconnect-8ecd3.firebaseapp.com",
+  projectId: "courtconnect-8ecd3",
+  storageBucket: "courtconnect-8ecd3.firebasestorage.app",
+  messagingSenderId: "1037514457918",
+  appId: "1:1037514457918:web:f5608f14d5facacda9e26f",
+};
+
+const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
